1df4666c5d4a94840182529afdb1db6cf5ed94fb
XiaoXinYo <1104361313@qq.com>
2023-12-05 15:42:15 +0800

    Update auxiliary.py

--- module/auxiliary.py
+++ module/auxiliary.py
@@ -1,4 +1,8 @@
 import re
+import time
+
+def getTimestamp(ms: bool=False) -> int:
+    return int(time.time() * 1 if ms else 1000)
 
 def isUrl(text: str) -> bool:
     re_ = re.compile(r'^((https|http|ftp|rtsp|mms)?:\/\/)[^\s]+')
@@ -16,3 +20,10 @@ def base62Encode(number: int) -> str:
         results.append(alphabet[rem])
     results.reverse()
     return ''.join(results)
+
+def snakeToSmallCamel(text):
+    texts = text.split('_')
+    return texts[0] + ''.join(word.capitalize() for word in texts[1:])
+
+def smallCamelToSnake(text):
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
