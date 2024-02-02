77199b71a317a1966bd89b56834bb0cb40de6edf
SunWuyuan <1847261658@qq.com>
2024-02-02 20:56:49 +0800

    1

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ HTTP = {
 }
 
 DATABASE = {
-    'host': os.environ.get('DATABASE_HOST') or '127.0.0.1', # 数据库地址
-    'port': int(os.environ.get('DATABASE_PORT', '3306')) or 3306, # 数据库端口
-    'username': os.environ.get('DATABASE_USERNAME') or 'root', # 数据库用户名
-    'password': os.environ.get('DATABASE_PASSWORD') or 'root', # 数据库密码
-    'name': os.environ.get('DATABASE_NAME') or 'h2o_short_url', # 数据库名称
-    'tablePrefix': os.environ.get('DATABASE_TABLE_PREFIX') or 'h2o_short_url_', # 数据库表前缀
+    'host': os.environ.get('DATABASE_HOST') or 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com', # 数据库地址
+    'port': int(os.environ.get('DATABASE_PORT', '3306')) or 4000, # 数据库端口
+    'username': os.environ.get('DATABASE_USERNAME') or '2nxmQMdzz7M77Sf.root', # 数据库用户名
+    'password': os.environ.get('DATABASE_PASSWORD') or '4NWwHgMQNQafvuQw', # 数据库密码
+    'name': os.environ.get('DATABASE_NAME') or 'url-database', # 数据库名称
+    'tablePrefix': os.environ.get('DATABASE_TABLE_PREFIX') or 'wuyuan_', # 数据库表前缀
     'ssl': {
         'enable': os.environ.get('DATABASE_SSL_ENABLE') or False, # 启用数据库SSL
         'caPath': os.environ.get('DATABASE_SSL_CA_PATH') or '', # 数据库SSL CA路径
