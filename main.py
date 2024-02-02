6505e976abb71c04e4f918515b4ba40a711e9ab0
孙悟元 <88357633+SunWuyuan@users.noreply.github.com>
2024-02-02 19:34:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ import flask_cors
 import config
 
 app = Flask(__name__)
-SSL = config.DATABASE.get('ssl')
-if SSL.get('enable'):
-    SSL_ = f'?ssl_ca={SSL["caPath"]}&ssl_key={SSL["keyPath"]}&ssl_cert={SSL["certPath"]}'
-else:
-    SSL_ = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{config.DATABASE["username"]}:{config.DATABASE["password"]}@{config.DATABASE["host"]}:{config.DATABASE["port"]}/{config.DATABASE["name"]}{SSL_}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{config.DATABASE["username"]}:{config.DATABASE["password"]}@{config.DATABASE["host"]}:{config.DATABASE["port"]}/{config.DATABASE["name"]}?ssl_ca=./isrgrootx1.pem&ssl_verify_cert=true&ssl_verify_identity=true'
 flask_cors.CORS(app, supports_credentials=True)
 app.register_blueprint(api.API_APP)
 app.register_blueprint(page.PAGE_APP)
@@ -32,11 +27,11 @@ def initialization() -> None:
     with app.app_context():
         model.DB.create_all()
         if not model.Core.query.filter_by(name='title').first():
-            model.DB.session.add(model.Core('title', '氧化氢短网址'))
+            model.DB.session.add(model.Core('title', '悟元短链接'))
         if not model.Core.query.filter_by(name='keyword').first():
-            model.DB.session.add(model.Core('keyword', '氧化氢短网址'))
+            model.DB.session.add(model.Core('keyword', '悟元短链接'))
         if not model.Core.query.filter_by(name='description').first():
-            model.DB.session.add(model.Core('description', '一切是那么的简约高效.'))
+            model.DB.session.add(model.Core('description', '缩短链接.'))
         model.DB.session.commit()
 
 initialization()
